358ae888eb05d943af2c920266624520e28c8af4
jackmillsgator <93085706+jackmillsgator@users.noreply.github.com>
2025-05-13 14:29:09 -0400

    Complete 10_leapYears in javascript exercises
    All tests passed.

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,4 +1,28 @@
-const leapYears = function() {
+const leapYears = function(yearArg)
+{
+
+    let isLeapYear = false;
+
+    if (yearArg % 4 === 0)
+    {
+
+        if (yearArg % 100 === 0)
+        {
+
+            if (yearArg % 400 === 0)
+            {
+                isLeapYear = true;
+            }
+
+        }
+        else
+        {
+            isLeapYear = true;
+        }
+
+    }
+
+    return isLeapYear;
 
 };
 
