1074fd7cca28a363dd70a1c29b94643a4b8c4960
jackmillsgator <93085706+jackmillsgator@users.noreply.github.com>
2025-05-13 14:58:53 -0400

    Complete 11_tempConversion in javascript exercises
    All tests passed.

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,33 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num)
+{
+
+    let numCels = 0;
+    numCels = (num - 32) * (5 / 9);
+
+    if (Number.isInteger(numCels) === false)
+    {
+        numCels = numCels.toFixed(1);
+        numCels = Number(numCels);
+    }
+
+    return numCels;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num)
+{
+
+    let numFahr = 0;
+    numFahr = num * (9 / 5) + 32;
+
+    if (Number.isInteger(numFahr) === false)
+    {
+        numFahr = numFahr.toFixed(1);
+        numFahr = Number(numFahr);
+    }
+
+    return numFahr;
+
 };
 
 // Do not edit below this line
