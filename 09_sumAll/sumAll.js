64c86dec9d09606fb7067dbdc6f0bda2e2a9046b
jackmillsgator <93085706+jackmillsgator@users.noreply.github.com>
2025-05-13 12:56:51 -0400

    Complete 09_sumAll in javascript exercises
    All tests passed.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,36 @@
-const sumAll = function() {
+const sumAll = function(num1, num2)
+{
+
+    let sum = 0;
+    let numLarger = 0;
+    let numSmaller = 0;
+
+    if (    (num1 < 0) ||
+            (num2 < 0) ||
+            (Number.isInteger(num1) === false) ||
+            (Number.isInteger(num2) === false)
+       )
+    {
+        return "ERROR";
+    }
+
+    if (num1 > num2)
+    {
+        numLarger = num1;
+        numSmaller = num2;
+    }
+    else
+    {
+        numLarger = num2;
+        numSmaller = num1;
+    }
+
+    for (let i = numSmaller; i <= numLarger; i++)
+    {
+        sum = sum + i;
+    }
+
+    return sum;
 
 };
 
