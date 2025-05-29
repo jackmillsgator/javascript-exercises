cfe542ed0b1fda24ce7e85ae6716ad2e49c99d53
jackmillsgator <93085706+jackmillsgator@users.noreply.github.com>
2025-05-29 17:50:02 -0400

    Complete Exercise 12 in javascript-exercises
    All tests have passed.

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,24 +1,78 @@
-const add = function() {
+const add = function(argA, argB)
+{
+
+    let answerNum = 0;
+
+    answerNum = argA + argB;
+
+    return answerNum;
 	
 };
 
-const subtract = function() {
+const subtract = function(argA, argB)
+{
+
+    let answerNum = 0;
+
+    answerNum = argA - argB;
+
+    return answerNum;
 	
 };
 
-const sum = function() {
+const sum = function(argArray)
+{
+
+    let answerNum = 0;
+
+    for (let i = 0; i < argArray.length; i++)
+    {
+        answerNum = answerNum + argArray[i];
+    }
+
+    return answerNum;
 	
 };
 
-const multiply = function() {
+const multiply = function(argArray)
+{
+
+    let answerNum = 1;
+
+    for (let i = 0; i < argArray.length; i++)
+    {
+        answerNum = answerNum * argArray[i];
+    }
+
+    return answerNum;
 
 };
 
-const power = function() {
+const power = function(argA, argB)
+{
+
+    let answerNum = 1;
+
+    for (let i = 0; i < argB; i++)
+    {
+        answerNum = answerNum * argA;
+    }
+
+    return answerNum;
 	
 };
 
-const factorial = function() {
+const factorial = function(argA)
+{
+
+    let answerNum = 1;
+
+    for (let i = 2; i <= argA; i++)
+    {
+        answerNum = answerNum * i;
+    }
+
+    return answerNum;
 	
 };
 
