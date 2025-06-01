a1bcf3c50093ef0dd8cd0032bbc2abbcf135074b
jackmillsgator <93085706+jackmillsgator@users.noreply.github.com>
2025-05-31 22:46:29 -0400

    Complete Exercise 14 in javascript-exercises
    Passes all tests.

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,4 +1,52 @@
-const fibonacci = function() {
+const fibonacci = function(argNumString)
+{
+
+    let fibSum = 1;
+    let prevNum1 = 1;
+    let prevNum2 = 1;
+
+    argNum = Number(argNumString);
+
+    if (argNum < 0)
+    {
+        return "OOPS";
+    }
+
+    if (argNum === 0)
+    {
+        return 0;
+    }
+
+    switch (argNum)
+    {
+
+        case 0:
+
+            return 0;
+            break;
+
+        case 1:
+
+            return 1;
+            break;
+
+        case 2:
+            return 1;
+            break;
+
+    }
+
+    for (let i = 2; i < argNum; i++)
+    {
+
+        fibSum = prevNum1 + prevNum2;
+
+        prevNum1 = prevNum2;
+        prevNum2 = fibSum;
+
+    }
+
+    return fibSum;
 
 };
 
