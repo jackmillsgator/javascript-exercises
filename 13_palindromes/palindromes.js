bc995037d7ad6d9044c83305f3a525734ef007b6
jackmillsgator <93085706+jackmillsgator@users.noreply.github.com>
2025-05-31 22:29:29 -0400

    Complete Exercise 13 in javascript-exercises
    All tests passed.

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,4 +1,26 @@
-const palindromes = function () {
+const palindromes = function (argString)
+{
+
+    let lowerCaseString = argString.toLowerCase();
+
+    let intermediatString1 = lowerCaseString.replaceAll(" ", "");
+    let intermediatString2 = intermediatString1.replaceAll(".", "");
+    let intermediatString3 = intermediatString2.replaceAll("!", "");
+    let intermediatString4 = intermediatString3.replaceAll("?", "");
+    let adjustedString = intermediatString4.replaceAll(",", "");
+
+    let argStringArray = adjustedString.split("");
+    let argStringArrayReversed = argStringArray.reverse();
+    let finalStringReversed = argStringArrayReversed.join("");
+
+    if (adjustedString === finalStringReversed)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 
 };
 
