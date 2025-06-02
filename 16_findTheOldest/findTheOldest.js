21e7f8d2b7b0a6cdb62479fd9fcd99903710c997
jackmillsgator <93085706+jackmillsgator@users.noreply.github.com>
2025-06-01 22:48:14 -0400

    Complete Exercise 16 in javascript-exercises
    Passed all tests.

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,4 +1,48 @@
-const findTheOldest = function() {
+const findTheOldest = function(arrayOfObjects)
+{
+
+    let oldestAge = 0;
+    let oldestName = "";
+
+    for (let i = 0; i < arrayOfObjects.length; i++)
+    {
+
+
+        let tempAge = 0;
+
+        if ("yearOfDeath" in arrayOfObjects[i])
+        {
+
+            if (arrayOfObjects[i].yearOfDeath !== undefined)
+            {
+                tempAge = Number(arrayOfObjects[i].yearOfDeath)
+                            - Number(arrayOfObjects[i].yearOfBirth);
+            }
+
+        }
+        else
+        {
+            tempAge = 2025
+                        - Number(arrayOfObjects[i].yearOfBirth);
+        }
+
+        if (tempAge > oldestAge)
+        {
+            oldestAge = tempAge;
+            oldestName = arrayOfObjects[i].name;
+        }
+
+    }
+
+    let returnedObject = 
+    {
+
+        age: oldestAge,
+        name: oldestName,
+
+    }
+
+    return returnedObject;
 
 };
 
